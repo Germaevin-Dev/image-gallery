3f58f927b40458b97bc196c5e80e4e050551e0bc
Germaevin-Dev <ndzegermaevin@gmail.com>
2026-02-28 09:03:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll(".filter-buttons button");
+const items = document.querySelectorAll(".gallery-item");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        // Remove active class from all buttons
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const filter = button.getAttribute("data-filter");
+
+        items.forEach(item => {
+            if (filter === "all" || item.classList.contains(filter)) {
+                item.classList.remove("hide");
+            } else {
+                item.classList.add("hide");
+            }
+        });
+
+    });
+});
